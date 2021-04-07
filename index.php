f5c1ae6fc1acff286459fc6803841de1c8ffe5f6
Papp László <papp.lazslo.web@gmail.com>
2021-04-07 13:59:57 +0200

    fix html

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 include('includes/head.php');
 
@@ -6,115 +6,115 @@ include('includes/head.php');
 
 <body>
 
-  
+
   <div class="container">
 
 
     <div class="row">
       <div class="col-md-12 mx-auto p-3">
-      <ul>
-    <script>
-      $(document).ready(function () {
+        <ul>
+          <script>
+            $(document).ready(function() {
 
-        $('#search').keyup(function () {
+              $('#search').keyup(function() {
 
-          var search = $('#search').val();
+                var search = $('#search').val();
 
 
-          $.ajax({
+                $.ajax({
 
-            url: 'search.php',
-            data: {
-              search: search
-            },
-            type: 'POST',
-            success: function (data) {
+                  url: 'search.php',
+                  data: {
+                    search: search
+                  },
+                  type: 'POST',
+                  success: function(data) {
 
-              if (!data.error) {
+                    if (!data.error) {
 
-                $('#result').html(data);
+                      $('#result').html(data);
+
+                    }
+
+
+                  }
+
+
+                });
+
+
+              });
+
+              setInterval(function() {
+
+                update();
+
+              }, 500);
+
+
+
+              // display 
+
+              function update() {
+
+                $.ajax({
+
+                  url: 'display.php',
+                  type: 'POST',
+                  success: function(show_fish) {
+
+                    if (!show_fish.error) {
+
+                      $("#show-data").html(show_fish);
+
+
+                    }
+
+                  }
+
+
+                });
 
               }
 
 
-            }
+
+              // ajax upload      
+              $("#add-fish-form").on('submit', (function(e) {
+                e.preventDefault();
+                $.ajax({
+                  url: "add.php",
+                  type: "POST",
+                  data: new FormData(this),
+                  contentType: false,
+                  cache: false,
+                  processData: false,
+                  beforeSend: function() {
+                    $("#preview").fadeOut();
+                    $("#err").fadeOut();
+                  },
+                  success: function(data) {
+                    if (data == 'invalid') {
+
+                      $("#err").html("Helytelen File !").fadeIn();
+                    } else {
 
 
-          });
-
-
-        });
-
-        setInterval(function () {
-
-          update();
-
-        }, 500);
-
-
-
-        // display 
-
-        function update() {
-
-          $.ajax({
-
-            url: 'display.php',
-            type: 'POST',
-            success: function (show_fish) {
-
-              if (!show_fish.error) {
-
-                $("#show-data").html(show_fish);
-
-
-              }
-
-            }
-
-
-          });
-
-        }
+                      $("#add-fish-form")[0].reset();
+                    }
+                  },
+                  error: function(e) {
+                    $("#err").html(e).fadeIn();
+                  }
+                });
+              }));
 
 
 
-        // ajax upload      
-        $("#add-fish-form").on('submit', (function (e) {
-          e.preventDefault();
-          $.ajax({
-            url: "add.php",
-            type: "POST",
-            data: new FormData(this),
-            contentType: false,
-            cache: false,
-            processData: false,
-            beforeSend: function () {
-              $("#preview").fadeOut();
-              $("#err").fadeOut();
-            },
-            success: function (data) {
-              if (data == 'invalid') {
+            });
+          </script>
 
-                $("#err").html("Helytelen File !").fadeIn();
-              } else {
-
-
-                $("#add-fish-form")[0].reset();
-              }
-            },
-            error: function (e) {
-              $("#err").html(e).fadeIn();
-            }
-          });
-        }));
-
-
-
-      });
-    </script>
-
-  </ul>
+        </ul>
 
 
         <div class="row">
@@ -125,84 +125,84 @@ include('includes/head.php');
           <br>
           <h2 class="result" id="result">
           </h2>
-        </div>
 
 
-        <form method="post" id="add-fish-form" class="col-md-8 mx-auto" action="add.php" enctype="multipart/form-data">
 
-          <div class="form-group">
-            <label for="post_img">Kép hozzáadása:</label>
-            <div class="box">
-              <img id="img-pre" src="#" alt="Saját kép"></img>
-              <div class="upload-options">
-                <label>
-                  <input type="file" class="form-control-file" id="imgInp" name="image" />
-                </label>
+          <form method="post" id="add-fish-form" class="col-md-8 mx-auto" action="add.php" enctype="multipart/form-data">
+
+            <div class="form-group">
+              <label for="post_img">Kép hozzáadása:</label>
+              <div class="box">
+                <img id="img-pre" src="#" alt="Saját kép"></img>
+                <div class="upload-options">
+                  <label>
+                    <input type="file" class="form-control-file" id="imgInp" name="image" />
+                  </label>
+                </div>
               </div>
             </div>
+            <div class="form-group">
+
+
+              <label for="water-name">Vízterületet neve:</label>
+
+              <input type="text" name="water_name" class="form-control" required>
+
+            </div>
+            <div class="form-group">
+
+
+              <label for="fish-name">Hal fajtája:</label>
+
+              <input type="text" name="fish_name" class="form-control" required>
+
+            </div>
+            <div class="form-group">
+
+
+              <label for="fish-weight">Hal súlya:</label>
+
+              <input type="text" name="fish_weight" class="form-control" required>
+
+            </div>
+
+
+
+            <input type="submit" class="btn btn-primary" value="Elküld" name="upload">
+
+
+          </form>
+
+
+        </div>
+
+        <div class="row">
+
+          <div class="col-md-12 mx-auto w-50 p-3">
+
+            <table class="table table-bordered table-hover">
+
+              <thead class="thead-dark">
+                <tr>
+
+                  <th>#</th>
+                  <th>Hal képe</th>
+                  <th>Vízterület</th>
+                  <th>Fajtája</th>
+                  <th>Súlya</th>
+                  <th>Dátum</th>
+                </tr>
+              </thead>
+              <tbody id="show-data"></tbody>
+            </table>
+
           </div>
-          <div class="form-group">
 
-
-            <label for="water-name">Vízterületet neve:</label>
-
-            <input type="text" name="water_name" class="form-control" required>
-
-          </div>
-          <div class="form-group">
-
-
-            <label for="fish-name">Hal fajtája:</label>
-
-            <input type="text" name="fish_name" class="form-control" required>
-
-          </div>
-          <div class="form-group">
-
-
-            <label for="fish-weight">Hal súlya:</label>
-
-            <input type="text" name="fish_weight" class="form-control" required>
-
-          </div>
-
-
-
-          <input type="submit" class="btn btn-primary" value="Elküld" name="upload">
-
-
-        </form>
-
+        </div>
       </div>
-    </div>
-
-    <div class="row">
-
-      <div class="col-md-12 mx-auto w-50 p-3">
-
-        <table class="table table-bordered table-hover">
-
-          <thead class="thead-dark">
-            <tr>
-
-              <th>#</th>
-              <th>Hal képe</th>
-              <th>Vízterület</th>
-              <th>Fajtája</th>
-              <th>Súlya</th>
-              <th>Dátum</th>
-            </tr>
-          </thead>
-          <tbody id="show-data"></tbody>
-        </table>
-
-      </div>
-
-    </div>
-  </div>
-<?php  
-include('includes/scripts.php');
-?>
+      <?php
+      include('includes/scripts.php');
+      ?>
 
 </body>
 
